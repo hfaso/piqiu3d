ae46b7d6f0859b1bff0cf3836427b333c63345e6
liujunli <1207654017@qq.com>
2020-01-20 14:29:03 +0800

    002

--- mylibs/base-utils.ts
+++ mylibs/base-utils.ts
@@ -1,12 +1,31 @@
-function initShaders (type: string) {
+function initShaders (type: string): void {
     
 }
 
-function loaderShaders(gl:object, type:string) :string {
+function createShaderProgram(){
+    
+}
+
+function loaderShaders(gl:object, type:string, source:string) :string {
     let shader:string = gl.createShader(type);
     if(shader === null){
         console.log("unable to create shader");
         return null;
     }
+
+    gl.shaderSource(shader, source);
+
+    gl.compileShader(shader);
+
+    let compiled = getShaderParameter(shader, gl.COMPILE_STATUS);
+    if(!compiled){
+        let errorLog = gl.getShaderInfoLog(shader);
+        console.log("Failed to compile shader" + errorLog);
+        gl.deleteShader(shader);
+        return null;
+    }
+
+
     return shader;
 }
+
